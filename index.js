89f98733a96678087c9eae7019c18f379605c2b2
james.yu <james.yu@gogoro.com>
2019-07-31 14:39:49 +0800

    1.4 Separate Express 'app' and 'server'

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log('This is how it all begins!')
+const app = require('./src/app')
+const port = 3000 // TODO: extract to env
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
