a029f0910613f4f2596de0f7fb9f7bb6f6f2bb56
james.yu <james.yu@gogoro.com>
2019-07-31 16:45:39 +0800

    User model

---
+++ src/components/users/User.js
@@ -0,0 +1,15 @@
+class User {
+  constructor (data) {
+    this.id = -1
+    this.name = data.name
+    this.age = data.age
+  }
+
+  isLegal () {
+    return this.age >= User.MIN_LEGAL_AGE
+  }
+}
+
+User.MIN_LEGAL_AGE = 21
+
+module.exports = User
